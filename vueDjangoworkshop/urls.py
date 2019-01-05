65e6820ee0a58a6f879097d05cc517f15f19c297
theo1201 <754173764@qq.com>
2019-01-05 15:05:26 +0800

    first commit

---
+++ vueDjangoworkshop/urls.py
@@ -0,0 +1,32 @@
+"""vueDjangoworkshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# from django.contrib import admin
+from django.urls import path,include,re_path
+# 导入django自带的server
+from  django.views.static import serve
+
+
+import xadmin
+
+urlpatterns = [
+    # 后台管理配置
+    path('xadmin/', xadmin.site.urls),
+    # 富文本配置
+    path('ueditor/', include('DjangoUeditor.urls')),
+    # 处理图片显示的url,使用Django自带serve,传入参数告诉它去哪个路径找，我们有配置好的路径MEDIAROOT
+    re_path('media/(?P<path>.*)', serve, {"document_root": MEDIA_ROOT }),
+
+]
